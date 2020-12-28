2f0ceaf9d5abd6973cdd79afad8dfb0167650a31
manjo95 <selva.manoj@gmail.com>
2020-12-28 13:53:07 +0000

    Adding controller and index page for Vet List.

---
+++ petclinicweb/src/main/java/com/example/petclinic/controllers/VetsController.java
@@ -0,0 +1,12 @@
+package com.example.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetsController {
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
