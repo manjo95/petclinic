ae6c9ab8745cfbc526f1823acbe1b6a58ebcdb5e
manjo95 <selva.manoj@gmail.com>
2020-12-28 14:13:10 +0000

    Adding controller and index page for Owners List.

--- petclinicweb/src/main/java/com/example/petclinic/controllers/OwnersController.java
+++ petclinicweb/src/main/java/com/example/petclinic/controllers/OwnersController.java
@@ -3,10 +3,11 @@ package com.example.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
-public class IndexController {
-    @RequestMapping({"","/index", "/index.html"})
-    public String index(){
-        return "index";
+public class OwnersController {
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOwners(){
+        return "/owners/index";
     }
 }
