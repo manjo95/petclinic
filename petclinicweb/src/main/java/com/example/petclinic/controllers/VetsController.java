47ec4ae4d6a1201ff3c2cc7907b9e3ecb82dde7f
manjo95 <selva.manoj@gmail.com>
2021-01-11 21:37:07 +0000

    Listing the Vets available in Vets page.

--- petclinicweb/src/main/java/com/example/petclinic/controllers/VetsController.java
+++ petclinicweb/src/main/java/com/example/petclinic/controllers/VetsController.java
@@ -1,12 +1,22 @@
 package com.example.petclinic.controllers;
 
+import com.example.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
+
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
